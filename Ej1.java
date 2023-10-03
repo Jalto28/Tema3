5bbae91fead5f4685f6e44045a4fdcca4b46d1a8
Jalto28 <58223109+Jalto28@users.noreply.github.com>
2023-10-03 16:03:24 +0200

    Update Ej1.java

--- Ej1.java
+++ Ej1.java
@@ -2,15 +2,19 @@ import java.util.Scanner;
 
 public class Ej1 {
     public static void main(String[] args) {
+        //Declaramos variables
         Scanner teclado = new Scanner(System.in);
+        int numero;
+        
+        //Pedimos el número teclado
+        System.out.print("Introduce un número: ");
+        numero = teclado.nextInt();
 
-        System.out.print("Ingrese un número: ");
-        int numero = scanner.nextInt();
-
+        //Comprobamos si es el resultado de una división entre 2 es 0
         if (numero % 2 == 0) {
-            System.out.println("El número ingresado es par.");
+            System.out.println("El número introducido es par.");
         } else {
-            System.out.println("El número ingresado es impar.");
+            System.out.println("El número introducido es impar.");
         }
 
         teclado.close();
