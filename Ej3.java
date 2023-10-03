2219e890420baffacbed8e49e9832648e7b752c6
Jalto28 <58223109+Jalto28@users.noreply.github.com>
2023-10-03 16:27:02 +0200

    Add files via upload

---
+++ Ej3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+		int edad;
+		
+		System.out.println("Introduce la edad");
+		edad = teclado.nextInt();
+		
+        if (edad >= 18) {
+            System.out.println("La persona de edad " + edad + " es mayor de edad");
+        } else if (edad <= 0) {
+            System.out.println("La edad no puede ser 0 o negativa");
+        } else if (edad > 0 && edad < 18) {
+            System.out.println("La persona de edad " + edad + " es menor de edad");
+        }
+    }
+}
