20ef47be52d2c9b647bbc7b099cb6523134712c2
Jalto28 <58223109+Jalto28@users.noreply.github.com>
2023-10-03 16:30:29 +0200

    Update Ej4.java

--- Ej4.java
+++ Ej4.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class Ej4 {
     public static void main(String[] args) {
         //Declaramos variables
-		Scanner teclado = new Scanner(System.in);
-		double numero1, numero2;
+	Scanner teclado = new Scanner(System.in);
+	double numero1, numero2, menor, mayor;;
 		
-		//Pedimos los numeros por teclado
+	//Pedimos los numeros por teclado
         System.out.print("Introduce el primer número real: ");
         numero1 = teclado.nextDouble();
 
         System.out.print("Introduce el segundo número real: ");
-        numero2 = teclado.nextDouble();
+        numero2 = teclado.nextDouble();        
 
-        double menor, mayor;
-
-        // Determinar el menor y el mayor de los dos números
+        // Determinamos el menor y el mayor de los dos números
         if (numero1 < numero2) {
             menor = numero1;
             mayor = numero2;
