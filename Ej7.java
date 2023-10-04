8bc7c0007c57d1601980071e3c2176615ea51273
Jalto28 <58223109+Jalto28@users.noreply.github.com>
2023-10-04 09:40:23 +0200

    Add files via upload

---
+++ Ej7.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Ej7 {
+
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Introduce la edad: ");
+        int edad = teclado.nextInt();
+
+        System.out.print("¿Los padres son socios? (S/N): ");
+        boolean padresSocios = teclado.nextBoolean();
+
+        double cuotaBase = 500;  // Cuota base en euros
+
+        // Calcular el descuento en función de la edad y si los padres son socios
+        double descuento = 0;
+
+        if (edad > 65) {
+            descuento = 0.5;  // 50% de descuento para personas mayores de 65 años
+        } else if (edad < 18) {
+            if (padresSocios) {
+                descuento = 0.35;  // 35% de descuento para menores de 18 años con padres socios
+            } else {
+                descuento = 0.25;  // 25% de descuento para menores de 18 años sin padres socios
+            }
+        }
+
+        // Calcular la cuota final después del descuento
+        double cuotaFinal = cuotaBase - (cuotaBase * descuento);
+
+        System.out.println("La cuota a abonar es: " + cuotaFinal + " euros");
+    }
+}
+
+
